16fb7757c5ae9c9c22ecc62c616c6dcf4a6a6682
FourTimes <57703276+FourTimes@users.noreply.github.com>
2020-09-08 13:41:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=> {
-	res.send(' This is node developemnt servers')
+	res.send(' This is node developemnts servers')
 })
 
 app.listen(8080, () => {
